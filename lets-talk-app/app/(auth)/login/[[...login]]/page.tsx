b3f9a379304928e9e798267f7404760d9203cb5a
MXMaddux <maxmaddux@gmail.com>
2025-06-19 05:15:17 -0700

    Get to Clerk login where only Google showing up.

---
+++ lets-talk-app/app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from "@clerk/nextjs";
+import { neobrutalism } from "@clerk/themes";
+
+const LoginPage = () => {
+  return (
+    <main>
+      <div>
+        <SignIn appearance={{ baseTheme: neobrutalism }} />
+      </div>
+    </main>
+  );
+};
+export default LoginPage;
---
+++ lets-talk-app/app/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <h1 className="font-black text-6xl text-center">Let's Talk App</h1>;
+}
